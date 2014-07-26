5cfccab4f537f9d4b1738cf8efc90fc7b95d1a53
Marcel Trapman <marceltrapman@it2be.com>
2014-07-26 15:27:47 +0200

    Enhanced plugin
    Added more error checking.
    Added encoding for title and message.
    Added support for priorities.

--- Contents/Server Plugin/plugin.py
+++ Contents/Server Plugin/plugin.py
@@ -1,54 +1,174 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+####################
+# Pushover plugin for Indigo 6
+#
+# Based on the work of Chad Francis (http://www.thechad.io)
+# Further Developed by Marcel Trapman and others.
+####################
+
 import httplib, urllib, sys, os
 
+
 class Plugin(indigo.PluginBase):
+    def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+        indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+        self.debug = True
 
-	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
-		indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
-		self.debug = True
-
-	def __del__(self):
-		indigo.PluginBase.__del__(self)
+    def __del__(self):
+        indigo.PluginBase.__del__(self)
 
 
-	def startup(self):
-		self.debugLog(u"startup called")
+    def startup(self):
+        self.debugLog(u"startup called")
 
-	def shutdown(self):
-		self.debugLog(u"shutdown called")
+    def shutdown(self):
+        self.debugLog(u"shutdown called")
 
-	# actions go here
-	def send(self, pluginAction):
-		pushtoken = self.pluginPrefs["applicationapikey"]
+    ########################################
+    # actions
+    ########################################
+    def send(self, pluginAction):
+        parameters = {}
 
-		if pluginAction.props["pushtoken"]:
-			pushtoken = pluginAction.props["pushtoken"]
+        try:
+            token = self.pluginPrefs["applicationapikey"]
 
-		pushtitle = pluginAction.props["txttitle"]
+            if "pushtoken" in pluginAction.props and pluginAction.props["pushtoken"]:
+                token = pluginAction.props["pushtoken"]
 
-		while "%%v:" in pushtitle:
-			pushtitle = self.substituteVariable(pushtitle)
-		
-		pushmessage = pluginAction.props["txtmessage"]
+            if not token:
+                raise Exception
 
-		while "%%v:" in pushmessage:
-			pushmessage = self.substituteVariable(pushmessage)
-		
-		conn = httplib.HTTPSConnection("api.pushover.net:443")
-		conn.request(
-			"POST",
-			"/1/messages",
-			urllib.urlencode({
-				"token": pushtoken,
-				"user": self.pluginPrefs["userkey"],
-				"title": pushtitle,
-				"message": pushmessage,
-				"device": pluginAction.props["pushdevice"],
-				"sound": pluginAction.props["pushsound"],
-				"url": pluginAction.props["pushurl"],
-				"url_title": pluginAction.props["pushurltitle"],
-			}),
-			{ "Content-type": "application/x-www-form-urlencoded" }
-		)
+            # Mandatory
+            parameters["token"] = token
+        except:
+            indigo.server.log(u"Mandatory token not set (correct)", isError = True)
+
+            return
+
+        try:
+            user = self.pluginPrefs["userkey"]
+
+            if not user:
+                raise Exception
+
+            # Mandatory
+            parameters["user"] = user
+        except:
+            indigo.server.log(u"Mandatory user not set", isError = True)
+
+            return
+
+        try:
+            message = u"%s", pluginAction.props["txtmessage"]
+
+            while "%%v:" in message:
+                message = self.substituteVariable(message)
+
+            # Mandatory
+            parameters["message"] = message
+        except:
+            indigo.server.log(u"Mandatory message not set", isError = True)
+
+            return
+
+        if "txttitle" in pluginAction.props and pluginAction.props["txttitle"]:
+            title = u"%s", pluginAction.props["txttitle"]
+
+            while "%%v:" in title:
+                title = self.substituteVariable(title)
+
+            # Optional
+            parameters["title"] = title
+
+        if "pushdevice" in pluginAction.props and pluginAction.props["pushdevice"]:
+            # Optional
+            parameters["device"] = pluginAction.props["pushdevice"]
+
+        if "pushsound" in pluginAction.props and pluginAction.props["pushsound"]:
+            # Optional
+            parameters["sourd"] = pluginAction.props["pushsound"]
+
+        if "pushurl" in pluginAction.props and pluginAction.props["pushurl"]:
+            # Optional
+            parameters["url"] = pluginAction.props["pushurl"]
+
+        if "pushurltitle" in pluginAction.props and pluginAction.props["pushurltitle"]:
+            # Optional
+            parameters["url_title"] = pluginAction.props["pushurltitle"]
+
+        if "priority" in pluginAction.props and pluginAction.props["priority"]:
+            try:
+                # Optional
+                priority = self.integerValue(pluginAction.props["priority"])
+
+                if 0 <> priority:
+                    parameters["priority"] = priority
+
+                    if priority == 2:
+                        try:
+                            retries = self.integerValue(pluginAction.props["retry"])
+
+                            if retries < 30:
+                                retries = 30
+
+                            # Mandatory when Emergency Priority is set
+                            parameters["retry"] = retries
+                        except:
+                            indigo.server.log(u"Mandatory Emergency retries not set (correct)", isError = True)
+
+                            return
+
+                        try:
+                            expire = self.integerValue(pluginAction.props["expire"])
+
+                            if expire > 86400:
+                                expire = 86400
+
+                            # Mandatory when Emergency Priority is set
+                            parameters["expire"] = expire
+                        except:
+                            indigo.server.log(u"Mandatory Emergency expiry not set (correct)", isError = True)
+
+                            return
+
+                        if "callback" in pluginAction.props and pluginAction.props["callback"]:
+                            # Optional when Emergency Priority is set
+                            parameters["callback"] = self.integerValue(pluginAction.props["callback"])
+            except:
+                indigo.server.log(u"Priority is not set (correct)", isError = True)
+
+                return
+
+        conn = httplib.HTTPSConnection("api.pushover.net:443")
+        conn.request(
+            "POST",
+            "/1/messages",
+            urllib.urlencode( parameters ),
+            {"Content-type": "application/x-www-form-urlencoded"})
+
+    ########################################
+    # Helper methods
+    ########################################
+    def integerValue(self, value):
+        try:
+            if isinstance(value, int):
+                return int(value)
+            elif isinstance(value, float):
+                return int(float(value + 0.5))
+            elif type(value) is bool:
+                if value:
+                    return 1
+                else:
+                    return 0
+            elif type(value) is str:
+                try:
+                    return int(value)
+                except ValueError:
+                    return int(float(value + 0.5))
+        except:
+            pass
+
+        return None
--- README.md
+++ README.md
@@ -9,10 +9,42 @@ indigo-pushover
 2. Valid Pushover [API key](https://pushover.net/api)
 3. Valid Pushover [user key](https://pushover.net/faq#overview-what)
 
-### Installation Instructions
-
+### Installation instructions release version
 1. Download latest release [here](https://github.com/discgolfer1138/indigo-pushover/releases)
 2. Follow [standard plugin installation process](http://bit.ly/1e1Vc7b)
 
+### Installation instruction repository version
+When you decide to download the plugin you will download a zipped archive named 'indigo-mysensors-master.zip'.
+Unzip the archive and rename the folder to 'MySensors.indigoPlugin'.
+
+When you have Indigo installed the folder will show as a single file (a so called package).
+When you doubleclick on the file you will automatically open Indigo (or bring it to the front) and you will be asked if you want to install and enable it.
+
 ### Actions Supported
-* Send Push Notification (subject and message are configurable)
+* Send Push Notification
+
+### Important
+Please check out the api documentation from Pushover when you want to know about the details but this is what was implemented:
+
+[Mandatory] configuration menu
+token - your application's API token (it is advised to set a default one to use here)
+user - the user/group key (not e-mail address), viewable when logged into our dashboard
+
+[Mandatory] trigger/action group
+token - your application's API token (when not set in the configuration menu it mandatory here)
+message - your message
+
+[Optional] trigger/action group
+token - your application's API token (when set in the configuration menu it is optional here)
+device - your user's device name to send the message directly to that device, rather than all of the user's devices
+title - your message's title, otherwise your app's name is used
+url - a supplementary URL to show with your message
+url title - a title for your supplementary URL, otherwise just the URL is shown
+priority - send as -2 to generate no notification/alert, -1 to always send as a quiet notification, 1 to display as high-priority and bypass the user's quiet hours, or 2 to also require confirmation from the user
+
+[When Emergency-priority is set]
+interval - the number of seconds between retries (minimum is 30 seconds, mandatory)
+expiry - the number of seconds after which you are not asked for confirmation anymore (maximum is 86400 seconds,mandatory)
+callback - publicly-accessible URL to which the confirmation will be sent (optional)
+
+
